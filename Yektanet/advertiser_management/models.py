974448ca6fce6597e586e716a537c3f4a44138e1
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-01-24 19:05:34 +0330

    Add methods to models and register them

--- Yektanet/advertiser_management/models.py
+++ Yektanet/advertiser_management/models.py
@@ -5,12 +5,23 @@ class BaseAdvertising(models.Model):
     clicks = models.IntegerField(verbose_name='Clicks', default=0)
     views = models.IntegerField(verbose_name='Views', default=0)
 
+    def inc_clicks(self):
+        self.clicks += 1
+        self.save()
+
+    def inc_views(self):
+        self.views += 1
+        self.save()
+
     class Meta:
         abstract = True
 
 
 class Advertiser(BaseAdvertising):
     name = models.CharField(max_length=70, verbose_name='Advertiser Name')
+
+    def __str__(self):
+        return self.name
 
 
 class Ad(BaseAdvertising):
@@ -21,5 +32,16 @@ class Ad(BaseAdvertising):
         to=Advertiser,
         on_delete=models.CASCADE,
         verbose_name='Advertiser',
-        related_name='ads',
+        related_name='ads'
     )
+
+    def inc_clicks(self):
+        super(Ad, self).inc_clicks()
+        self.advertiser.inc_clicks()
+
+    def inc_views(self):
+        super(Ad, self).inc_views()
+        self.advertiser.inc_views()
+
+    def __str__(self):
+        return self.title
