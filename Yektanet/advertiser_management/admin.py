3b48270bd615b50ea0a1dedcc347ee40d16fbcd6
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-01-27 02:07:46 +0330

    admin page search, filter and mark approved abilities added

--- Yektanet/advertiser_management/admin.py
+++ Yektanet/advertiser_management/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Advertiser, Ad, Click, View
 
-
 admin.site.register(Advertiser)
-admin.site.register(Ad)
 admin.site.register(Click)
 admin.site.register(View)
+
+
+@admin.register(Ad)
+class AdModel(admin.ModelAdmin):
+    list_display = ['title', 'approve']
+    list_filter = (('approve', admin.BooleanFieldListFilter),)
+    search_fields = ['title']
+    actions = ['mark_approved', 'mark_not_approved']
+
+    def mark_approved(self, request, queryset):
+        queryset.update(approve=True)
+
+    def mark_not_approved(self, request, queryset):
+        queryset.update(approve=False)
+
+    mark_approved.short_description = 'Mark selected ads as approved'
+    mark_not_approved.short_description = 'Mark selected ads as not approved'
