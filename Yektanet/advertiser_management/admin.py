98a8032a912ed947269aca41ba8fa20fd3707301
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-01-26 20:59:13 +0330

    Change models and add Click and View models

--- Yektanet/advertiser_management/admin.py
+++ Yektanet/advertiser_management/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Advertiser, Ad
+from .models import Advertiser, Ad, Click, View
 
 
 admin.site.register(Advertiser)
 admin.site.register(Ad)
+admin.site.register(Click)
+admin.site.register(View)
--- Yektanet/advertiser_management/models.py
+++ Yektanet/advertiser_management/models.py
@@ -1,30 +1,14 @@
 from django.db import models
 
 
-class BaseAdvertising(models.Model):
-    clicks = models.IntegerField(verbose_name='Clicks', default=0)
-    views = models.IntegerField(verbose_name='Views', default=0)
-
-    def inc_clicks(self):
-        self.clicks += 1
-        self.save()
-
-    def inc_views(self):
-        self.views += 1
-        self.save()
-
-    class Meta:
-        abstract = True
-
-
-class Advertiser(BaseAdvertising):
+class Advertiser(models.Model):
     name = models.CharField(max_length=70, verbose_name='Advertiser Name')
 
     def __str__(self):
         return self.name
 
 
-class Ad(BaseAdvertising):
+class Ad(models.Model):
     title = models.CharField(max_length=50, verbose_name='Ad Title')
     image = models.CharField(max_length=2000, verbose_name='Image Address')
     link = models.CharField(max_length=2000, verbose_name='Link')
@@ -35,13 +19,31 @@ class Ad(BaseAdvertising):
         related_name='ads'
     )
 
-    def inc_clicks(self):
-        super(Ad, self).inc_clicks()
-        self.advertiser.inc_clicks()
-
-    def inc_views(self):
-        super(Ad, self).inc_views()
-        self.advertiser.inc_views()
-
     def __str__(self):
         return self.title
+
+
+class BaseAdInfo(models.Model):
+    click_time = models.DateTimeField(auto_now_add=True, verbose_name='Time')
+    user_ip = models.GenericIPAddressField(verbose_name='User IP')
+
+    class Meta:
+        abstract = True
+
+
+class Click(BaseAdInfo):
+    ad = models.ForeignKey(
+        to=Ad,
+        on_delete=models.CASCADE,
+        verbose_name='Clicked Ad',
+        related_name='clicks'
+    )
+
+
+class View(BaseAdInfo):
+    ad = models.ForeignKey(
+        to=Ad,
+        on_delete=models.CASCADE,
+        verbose_name='Viewed Ad',
+        related_name='views'
+    )
